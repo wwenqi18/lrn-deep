7de43f853b7a5d68d2efd94d7b82c94b0ee2c7ff
Qing Teng <qt2126@columbia.edu>
2018-11-02 10:26:25 -0400

    Silence pylint alerts for conventions

--- canvas/views.py
+++ canvas/views.py
@@ -3,7 +3,9 @@
 from django.shortcuts import render
 
 def index(request):
+    """ Return index page """
     return render(request, 'canvas/index.html')
 
 def workspace(request):
+    """ Return workspace page """
     return render(request, 'canvas/workspace.html')
